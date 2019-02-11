5b51a57495db8d0e6887ee6c7e77cc82cb4c5692
CHEOL <mihocc@naver.com>
2019-02-11 16:59:10 +0900

    1

--- kocofarm01/src/main/java/org/kocofarm/mapper/module/ApprovalMapper.java
+++ kocofarm01/src/main/java/org/kocofarm/mapper/module/ApprovalMapper.java
@@ -2,67 +2,62 @@ package org.kocofarm.mapper.module;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
 import org.kocofarm.domain.approval.ApprDraftVO;
-import org.kocofarm.domain.approval.ApprExpenceContVO;
-import org.kocofarm.domain.approval.ApprExpenceVO;
+import org.kocofarm.domain.approval.ApprFormVO;
 import org.kocofarm.domain.approval.ApprVacationVO;
 
 public interface ApprovalMapper {
+	/* 기본기안서 입력 */
+	public void insertDraft(ApprDraftVO draft);
 
-	//기안서입력
-	public void InsertDraft(ApprDraftVO draft);
-	//휴가정보입력
-	public void InsertVacation(ApprVacationVO vacation);
-	//지출내역입력
-	public void InsertExpence(ApprExpenceVO expence);
-	//지출양식입력
-	public void InsertExpenceCont(ApprExpenceContVO expenceCont);
-	//양식출력
-	
-	//전체기안서출력
-	@Select("select * from DRAFT where DRAFT_ID > 0")
-	List<ApprDraftVO> getListDraft();
-	
+	public void insertDraftSelectKey(ApprDraftVO draft);
 
-//	/*양식 출력*/
-////	List<ApprovalForm> listForm(ApprovalFormSearch search);
-//	List<ApprovalForm> listForm();
-//	/*전체 기안서 출력*/ 
-//	List<ApprovalDraft> listDraft();
-//	/*분류된 기안서 출력*/ 
-//	List<ApprovalDraft> listStateDraft();
-//	/*기안서 상세 입력 폼 이동*/
-//	ApprovalForm getDetailForm(int formId);
-//	/*emp정보 불러오기*/
-//	int getDraftId();
-//	/*지출결의서 번호 불러오기*/
-//	int getExpenceId();
-//	/*기본 기안서 정보 불러오기*/
-//	ApprovalDraft getDraft(int draftId);
-//	/*휴가 신청서 불러오기*/
-//	ApprovalVacation getVacation(int draftId);
-//	/*지출 결의서 불러오기 */
-//	ApprovalExpence getExpence(int draftId);
-//	/*지출 내용 정보 불러오기*/
-//	List<ApprovalExpenceCont> getExpenceCont(int expenceId);
-//	/*기본 기안서 정보 수정*/
-//	int updateDraft(ApprovalDraft draft);
-//	/*휴가 신청서 정보 수정*/
-//	int updateVacation(ApprovalVacation vacation);
-//	/*기본 기안서 정보 삭제*/
-//	int deleteDraft(int draftId);
-//	/*휴가 신청서 삭제*/
-//	int deleteVacation(int draftId);
-//	/*지출 결의서 삭제*/
-//	int deleteExpence(int draftId);
-//	/*댓글 등록*/
-//	int insertComment(DraftComment comment);
-//	/*댓글 목록*/
-//	List<DraftComment> listComment(int draftId);
-//	
-//	List<DraftComment> getDraftCommentList(Map<String, Object> map);
-//	
-//	int getCommentCount(Map<String, Object> map);
+	/* 휴가 정보 입력 */
+	int insertVacation(ApprVacationVO vacation);
+
+	/* 양식 출력 */
+	//// List<ApprovalForm> listForm(ApprovalFormSearch search);
+	List<ApprFormVO> listForm();
+
+	// 전체기안서출력
+	// @Select("select * from DRAFT where DRAFT_ID > 0")
+	// List<ApprDraftVO> listDraft();
+
+	// /*전체 기안서 출력*/
+	// List<ApprovalDraft> listDraft();
+	// /*분류된 기안서 출력*/
+	// List<ApprovalDraft> listStateDraft();
+	// /*기안서 상세 입력 폼 이동*/
+	// ApprovalForm getDetailForm(int formId);
+	// /*emp정보 불러오기*/
+	// int getDraftId();
+	// /*지출결의서 번호 불러오기*/
+	// int getExpenceId();
+	// /*기본 기안서 정보 불러오기*/
+	// ApprovalDraft getDraft(int draftId);
+	// /*휴가 신청서 불러오기*/
+	// ApprovalVacation getVacation(int draftId);
+	// /*지출 결의서 불러오기 */
+	// ApprovalExpence getExpence(int draftId);
+	// /*지출 내용 정보 불러오기*/
+	// List<ApprovalExpenceCont> getExpenceCont(int expenceId);
+	// /*기본 기안서 정보 수정*/
+	// int updateDraft(ApprovalDraft draft);
+	// /*휴가 신청서 정보 수정*/
+	// int updateVacation(ApprovalVacation vacation);
+	// /*기본 기안서 정보 삭제*/
+	// int deleteDraft(int draftId);
+	// /*휴가 신청서 삭제*/
+	// int deleteVacation(int draftId);
+	// /*지출 결의서 삭제*/
+	// int deleteExpence(int draftId);
+	// /*댓글 등록*/
+	// int insertComment(DraftComment comment);
+	// /*댓글 목록*/
+	// List<DraftComment> listComment(int draftId);
+	//
+	// List<DraftComment> getDraftCommentList(Map<String, Object> map);
+	//
+	// int getCommentCount(Map<String, Object> map);
 
 }
--- kocofarm01/src/test/java/org/kocofarm/mapper/module/TestApprovalMapper.java
+++ kocofarm01/src/test/java/org/kocofarm/mapper/module/TestApprovalMapper.java
@@ -2,6 +2,8 @@ package org.kocofarm.mapper.module;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kocofarm.domain.approval.ApprDraftVO;
+import org.kocofarm.domain.approval.ApprVacationVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,8 +20,34 @@ public class TestApprovalMapper {
 	private ApprovalMapper mapper;
 	
 	@Test
-	public void testGetList(){
-		mapper.getListDraft().forEach(draft -> log.info(draft));
+//	public void testGetList(){
+//		mapper.listDraft().forEach(draft -> log.info(draft));
+//	}
+//	public void testListForm(){
+//		mapper.listForm().forEach(form -> log.info(form));
+//	}
+//	public void testInsertDraft(){
+//		ApprDraftVO draft = new ApprDraftVO();
+//		draft.setDraftName("¿¹½Ã");
+//		draft.setDraftTitle("¿¹½ÃÀÔ´Ï´Ù");
+//		draft.setEmpId("EMP_001");
+//		draft.setDraftYear(3);
+//		draft.setFormId(4);
+//		draft.setApproveState("°áÁ¦Áß");
+//		
+//		mapper.insertDraft(draft);
+//		log.info(draft);
+//	}
+	public void testInsertVacation(){
+		ApprVacationVO vacation = new ApprVacationVO();
+		vacation.setDraftId(42);
+		vacation.setFormId(4);
+		vacation.setVacationType("¾Æ¸ô¶û½¯·©");
+		vacation.setVacationDays(1);
+		vacation.setVacationReason("¾Æ¸ô¶û");
+		vacation.setReplacementId("EMP_001");
+		
+		mapper.insertVacation(vacation);
+		log.info(vacation);
 	}
-	
 }
