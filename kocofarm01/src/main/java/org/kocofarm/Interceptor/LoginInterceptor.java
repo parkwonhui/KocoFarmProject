3cd0684bc982b95ec7fe3ab0a74555a5fd118306
Aram <iiknow83@gmail.com>
2019-02-20 17:44:22 +0900

    20190220 update

--- kocofarm01/src/main/java/org/kocofarm/Interceptor/LoginInterceptor.java
+++ kocofarm01/src/main/java/org/kocofarm/Interceptor/LoginInterceptor.java
@@ -9,35 +9,33 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter {
-	 @Override
-	    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-	            throws Exception {
-	        
-	        HttpSession session = request.getSession();
-	        LoginVO loginVO = (LoginVO) session.getAttribute("loginVO");
-	 
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
 
-	        if(loginVO == null){
-	            response.sendRedirect("/signIn/page");
-	            return false;
-	        }
+		HttpSession session = request.getSession();
+		LoginVO loginVO = (LoginVO) session.getAttribute("loginVO");
 
-	        return true;
-	    }
-	 
-	    @Override
-	    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-	            ModelAndView modelAndView) throws Exception {
-	        
-	        super.postHandle(request, response, handler, modelAndView);
-	    }
-	 
-	    @Override
-	    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-	            throws Exception {
-	 
-	        super.afterCompletion(request, response, handler, ex);
-	    }
-	 
-	    
+		if (loginVO == null) {
+			response.sendRedirect("/signIn/page");
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
+		super.afterCompletion(request, response, handler, ex);
+	}
+
 }
